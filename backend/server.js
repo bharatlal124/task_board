21d1c0a86e35d24dd772503eff7b7f489ef25e91
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-05-23 14:35:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173", "https://flourishing-travesseiro-38ae34.netlify.app/"],
     methods: ["GET", "POST"],
+     credentials: true
   },
 });
 
