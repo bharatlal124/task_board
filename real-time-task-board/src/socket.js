4832de159fc4a60e12086788587ac3865f42d6a8
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-05-23 13:48:48 +0530

    Update socket.js

--- real-time-task-board/src/socket.js
+++ real-time-task-board/src/socket.js
@@ -1,7 +1,7 @@
 //socket.js
 import { io } from "socket.io-client";
 
-const socket = io("https://real-time-task-board-y3lm.onrender.com");
+// const socket = io("https://real-time-task-board-y3lm.onrender.com");
 const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 export default socket;
