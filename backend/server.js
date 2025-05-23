61accce82e7c558f1a689aae261acc027496ed26
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-05-23 13:43:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
